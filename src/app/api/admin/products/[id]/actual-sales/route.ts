760e454dfaffaa9d9eefec0339b563ffb138729c
Dhanush D <137797395+St4bl3@users.noreply.github.com>
2024-11-26 10:30:44 +0530

    god knows why its not able to host

--- src/app/api/admin/products/[id]/actual-sales/route.ts
+++ src/app/api/admin/products/[id]/actual-sales/route.ts
@@ -9,7 +9,10 @@ const prisma = new PrismaClient();
 // Define Admin User ID
 const ADMIN_USER_ID = "user_2pKg9sDw4aGoiVfvwWfquJDWK5C"; // Replace with your actual admin user ID
 
-export async function GET(request: NextRequest) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const { userId: authenticatedUserId } = getAuth(request);
 
@@ -18,9 +21,7 @@ export async function GET(request: NextRequest) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    // Extract the product ID from the URL using request.url
-    const url = new URL(request.url);
-    const productId = url.pathname.split("/")[5]; // This should be the dynamic part of the URL
+    const productId = params.id; // Access the dynamic route parameter directly from params
 
     if (!productId) {
       return NextResponse.json(
